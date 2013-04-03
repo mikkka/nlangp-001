fb6e97253b66ac609e593af5b06f0e6dc5076411
mick <mick@z-inc.net>
2013-04-03 23:16:38 +0600

    - filter and toList

--- pas/pa2/src/main/scala/org/mtkachev/nlangp/pa2/Tree.scala
+++ pas/pa2/src/main/scala/org/mtkachev/nlangp/pa2/Tree.scala
@@ -7,11 +7,26 @@ package org.mtkachev.nlangp.pa2
  */
 abstract class Node {
   val tag: String
+
+  def filter(f: Node => Boolean): List[Node]
+  def toList(): List[Node]
 }
 
 case class Terminal(tag: String, word: String) extends Node {
+  def filter(f: (Node) => Boolean): List[Node] =
+    if (f(this)) List(this)
+    else Nil
+
+  def toList(): List[Node] = List(this)
+
   override def toString = s"""[$tag, $word]"""
 }
 case class NonTerminal(tag: String, lft: Node, rgt: Node) extends Node {
+  def filter(f: (Node) => Boolean): List[Node] =
+    if (f(this)) this :: (lft.filter(f) ::: rgt.filter(f))
+    else lft.filter(f) ::: rgt.filter(f)
+
+  def toList(): List[Node] = this :: (lft.toList() ::: rgt.toList())
+
   override def toString = s"""[$tag, $lft, $rgt]"""
 }
