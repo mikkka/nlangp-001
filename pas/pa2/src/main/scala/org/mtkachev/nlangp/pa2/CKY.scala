b2b8fbc1131430fc533b336708d4671130082d9c
mick <mick@z-inc.net>
2013-04-08 22:14:19 +0600

    api change

--- pas/pa2/src/main/scala/org/mtkachev/nlangp/pa2/CKY.scala
+++ pas/pa2/src/main/scala/org/mtkachev/nlangp/pa2/CKY.scala
@@ -17,7 +17,7 @@ object CKY {
       i <- 1 to sentence.length;
       tag <- params.allTerminalTags
     ) {
-      pi.put((i, i, tag), (params.q(tag, word(i)), Terminal(tag, word(i))))
+      pi.put((i, i, tag), (params.q(UnaryRule(tag, word(i))), Terminal(tag, word(i))))
     }
 
     //pi table computing
@@ -33,7 +33,7 @@ object CKY {
       ) yield {
         val pi1 = pi.getOrElse((i, s, rule.to1), (0.0, null))
         val pi2 = pi.getOrElse(((s + 1, j, rule.to2)), (0.0, null))
-        if (pi1._1 == 0.0 || pi2._2 == 0.0) (0.0, null)
+        if (pi1._1 == 0.0 || pi2._1 == 0.0) (0.0, null)
         else ((params.q(rule) * pi1._1 * pi2._1), NonTerminal(rule.from, pi1._2, pi2._2))
       }).maxBy(_._1)
 
