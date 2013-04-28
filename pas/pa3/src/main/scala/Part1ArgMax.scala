2e69cb3669f0c10f84f8c6d6d0e59aeeca0f5f35
mick <mick@z-inc.net>
2013-04-28 21:11:37 +0600

    - arg max implementation

--- pas/pa3/src/main/scala/Part1ArgMax.scala
+++ pas/pa3/src/main/scala/Part1ArgMax.scala
@@ -5,6 +5,19 @@
  */
 object Part1ArgMax extends App {
   val params1 = Params1(args(0))
+  val corpus = new Corpus(args(1), args(2))
 
-  print(params1.es)
+  val aligns = (for (
+    tEF <- corpus.zipped.zipWithIndex
+  ) yield {
+    val eSentence = tEF._1._1
+    val fSentence = tEF._1._2
+    val i = tEF._2
+
+    fSentence.zipWithIndex.map{fWord =>
+      (fWord, eSentence.zipWithIndex.maxBy(eWord => params1.t(fWord._1, eWord._1)))
+    }
+  })
+
+  println(aligns)
 }
