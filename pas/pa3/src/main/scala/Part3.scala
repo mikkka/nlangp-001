6a6b65003b496da60f15eeeceef25718349d444f
mick <mick@z-inc.net>
2013-04-29 23:01:33 +0600

    - adding neighbour nodes from union

--- pas/pa3/src/main/scala/Part3.scala
+++ pas/pa3/src/main/scala/Part3.scala
@@ -13,24 +13,47 @@ object Part3 extends App {
   val params2EsEn = Params2(args(5))
 
   val alignsEnEs = argmaxAligns(corpusEnEs, params1EnEs, params2EnEs)
-  val alignsEsEn = argmaxAligns(corpusEsEn, params1EsEn, params2EsEn)
+  val alignsEsEn = argmaxAligns(corpusEsEn, params1EsEn, params2EsEn).map(es => es.map(t => (t._1, t._3, t._2)))
 
-  val alignIntersection =
+  val aligns = (
     for(
-      (ens, ess) <- alignsEnEs.zip(alignsEsEn);
-      en <- ens;
-      es <- ess
-      if (en._2 == es._3 && en._3 == es._2)
-    ) yield (en)
+      (ens, ess) <- alignsEnEs.zip(alignsEsEn)
+    ) yield {
+      //aligns intersection init
+      val alignMap = (for (
+        en <- ens;
+        es <- ess
+        if (en._2 == es._2 && en._3 == es._3)
+      ) yield (en)).foldLeft(Map.empty[Int, Map[Int, Int]]) {(acc, a) =>
+        val subMap = acc.getOrElse(a._2, Map.empty[Int, Int]) + (a._3 -> a._1)
+        acc + (a._2 -> subMap)
+      }
+
+      //fold left on alignments union with intersection as starting point
+      ((ens ++ ess ++ ens ++ ess ++ ens ++ ess)).foldLeft(alignMap){(acc, a) =>
+        if (
+          acc.getOrElse(a._2 - 1 , Map.empty[Int, Int]).contains(a._3)
+            || acc.getOrElse(a._2 + 1, Map.empty[Int, Int]).contains(a._3)
+            || acc.getOrElse(a._2, Map.empty[Int, Int]).contains(a._3 - 1)
+            || acc.getOrElse(a._2, Map.empty[Int, Int]).contains(a._3 + 1)
+            || acc.getOrElse(a._2 + 1, Map.empty[Int, Int]).contains(a._3 + 1)
+            || acc.getOrElse(a._2 + 1, Map.empty[Int, Int]).contains(a._3 - 1)
+            || acc.getOrElse(a._2 - 1, Map.empty[Int, Int]).contains(a._3 + 1)
+            || acc.getOrElse(a._2 - 1, Map.empty[Int, Int]).contains(a._3 - 1)
+        ) {
+          val subMap = acc.getOrElse(a._2, Map.empty[Int, Int]) + (a._3 -> a._1)
+          acc + (a._2 -> subMap)
+        } else acc
+      }.flatMap(kv => kv._2.map(t => (t._2, kv._1, t._1)))
+    })
 
   FileIO.withPrintWriter(args(6)){pw =>
     for (
-      (i, from, to) <- alignIntersection
+      (i, from, to) <- aligns.flatten
     ) {
       pw.println(s"$i $from $to")
     }
   }
-
 
   def argmaxAligns(corpus: Corpus, params1: Params1, params2: Params2) = {
     val aligns = (for (
