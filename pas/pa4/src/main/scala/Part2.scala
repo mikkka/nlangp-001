d9d3edc5d1cd92a1d7c63eb3220bcb8dffbc8473
mick <mick@z-inc.net>
2013-05-12 17:47:13 +0600

    - part2 with suffixes

--- pas/pa4/src/main/scala/Part2.scala
+++ pas/pa4/src/main/scala/Part2.scala
@@ -10,15 +10,20 @@ object Part2 extends App {
 
   val trigramFeatures = new TrigramsFeatures
   val tagFeatures = new TagFeatures
+  val suffix1features = new SuffixFeatures(1)
+  val suffix2features = new SuffixFeatures(2)
+  val suffix3features = new SuffixFeatures(3)
   //find all features in input
 
   ss.foreach{xs =>
     trigramFeatures.findFeatures(xs)
     tagFeatures.findFeatures(xs)
+    suffix1features.findFeatures(xs)
+    suffix2features.findFeatures(xs)
+    suffix3features.findFeatures(xs)
   }
 
-  trigramFeatures.shift(0)
-  tagFeatures.shift(trigramFeatures.size)
+  suffix3features.shift(suffix2features.shift(suffix1features.shift(tagFeatures.shift(trigramFeatures.shift(0)))))
 
   val v = Algs.perceptron(ss, Set(trigramFeatures, tagFeatures))
 
